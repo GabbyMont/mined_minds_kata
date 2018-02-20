3f5bb70f3b4dff06d4dd7d7d2cfa2a8d8925ce1f
GabbyMont <gemo1236@gmail.com>
2018-02-20 15:34:45 -0500

    test update

--- test_mindminedkata.rb
+++ test_mindminedkata.rb
@@ -1,13 +1,16 @@
 require "minitest/autorun"
-# require_relative "test_fuction.rb"
+require_relative "test_fuction.rb"
 
-class TestFuctionTest < Minitest::Test
-	def test_bool
+class Test_Fuction_test < Minitest::Test
+	def test_
 		assert_equal(true,true)
 	end
+
 	def test_1_returns_1
 		assert_equal(1, minedminds(1))
 	end
+
 	def test_7_returns_7
 		assert_equal(7, minedminds(7))
+	end
 end
