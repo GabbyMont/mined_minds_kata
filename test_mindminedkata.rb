284274ed478e7f8b0506bf00bd61b94ac72cd46f
GabbyMont <gemo1236@gmail.com>
2018-02-22 15:38:33 -0500

    Test_1_returns_1

--- test_mindminedkata.rb
+++ test_mindminedkata.rb
@@ -6,9 +6,9 @@ class Test_Fuction_test < Minitest::Test
 		assert_equal(true,true)
 	end
 
-	# def test_1_returns_1
-	# 	assert_equal(1, minedminds(1))
-	# end
+	def test_1_returns_1
+		assert_equal(1, minedminds(1))
+	end
 
 	# def test_7_returns_7
 # 		assert_equal(7, minedminds(7))
