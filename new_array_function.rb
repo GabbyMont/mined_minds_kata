837bb28149e1ae0750d07515d8ecaf1c8c706c4c
GabbyMont <gemo1236@gmail.com>
2018-02-26 23:38:12 -0500

    Test_1_equals_1

---
+++ new_array_function.rb
@@ -0,0 +1,18 @@
+def arraynew()
+	thearray = []
+	x = 1
+
+	while x <= 100
+		if x % 3 == 0 && x % 5 == 0
+			thearray.push("Mined Minds")
+		elsif x % 3 == 0
+			thearray.push("Mined")
+		elsif x % 5 == 0
+			thearray.push("Minds")
+		else
+			thearray.push(x)
+		end
+		x += 1
+	end
+	thearray
+end
